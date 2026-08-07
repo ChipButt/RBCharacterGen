6e12ad80032e81231bb953d72739bddf19f29454
ChipButt <jameschipbutt@hotmail.com>
2026-08-07 10:44:20 +0100

    Register offline mode immediately

--- offline.js
+++ offline.js
@@ -1,15 +1,9 @@
 (()=>{
   if(!('serviceWorker' in navigator)) return;
 
-  const register=async()=>{
-    try{
-      const registration=await navigator.serviceWorker.register('./sw.js',{scope:'./'});
-      registration.update().catch(()=>{});
-    }catch(err){
-      console.error('Offline mode could not be enabled:',err);
-    }
-  };
-
-  if(document.readyState==='complete') register();
-  else window.addEventListener('load',register,{once:true});
+  navigator.serviceWorker.register('./sw.js',{scope:'./'}).then(registration=>{
+    registration.update().catch(()=>{});
+  }).catch(err=>{
+    console.error('Offline mode could not be enabled:',err);
+  });
 })();
