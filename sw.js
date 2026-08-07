ea60fa32ac3c692b9261bbc3b6adae247f31b255
ChipButt <jameschipbutt@hotmail.com>
2026-08-07 10:43:08 +0100

    Add full offline service worker

---
+++ sw.js
@@ -0,0 +1,133 @@
+const CACHE_NAME='rb600-offline-v1';
+
+const PRECACHE=[
+  './',
+  './index.html',
+  './player.html',
+  './gm.html',
+  './offline.js',
+  './600_second_dnd_character_player_v21.html',
+  './600 Second logo.png',
+  './gm_v3.css',
+  './gm_v3.js',
+  './gm_fit.js',
+  './gm_export.js',
+  './gm_export_builder.js',
+
+  './600_Second_DnD_Individual_Assets/01_roll_britannia_logo.png',
+  './600_Second_DnD_Individual_Assets/02_600_second_dnd_title.png',
+  './600_Second_DnD_Individual_Assets/03_top_brush_stroke.png',
+  './600_Second_DnD_Individual_Assets/04_middle_brush_stroke.png',
+  './600_Second_DnD_Individual_Assets/05_small_left_pill.png',
+  './600_Second_DnD_Individual_Assets/06_small_center_pill.png',
+  './600_Second_DnD_Individual_Assets/07_small_right_pill.png',
+  './600_Second_DnD_Individual_Assets/08_bottom_brush_stroke.png',
+
+  './DnD_Puzzle_Cards_10/01_the_missing_torch.png',
+  './DnD_Puzzle_Cards_10/02_the_three_buttons.png',
+  './DnD_Puzzle_Cards_10/03_the_silent_bell.png',
+  './DnD_Puzzle_Cards_10/04_the_candle_order.png',
+  './DnD_Puzzle_Cards_10/05_the_empty_pedestal.png',
+  './DnD_Puzzle_Cards_10/06_the_shadow_key.png',
+  './DnD_Puzzle_Cards_10/07_the_numbered_stones.png',
+  './DnD_Puzzle_Cards_10/08_the_sleeping_face.png',
+  './DnD_Puzzle_Cards_10/09_the_upside_down_message.png',
+  './DnD_Puzzle_Cards_10/10_the_four_seasons.png',
+
+  './DnD_Monster_Stat_Cards_10/01_Cave_Gobbler.png',
+  './DnD_Monster_Stat_Cards_10/02_Skeleton_Guard.png',
+  './DnD_Monster_Stat_Cards_10/03_Giant_Dungeon_Rat.png',
+  './DnD_Monster_Stat_Cards_10/04_Bog_Slime.png',
+  './DnD_Monster_Stat_Cards_10/05_Kobold_Bomb_Thrower.png',
+  './DnD_Monster_Stat_Cards_10/06_Graveyard_Ghoul.png',
+  './DnD_Monster_Stat_Cards_10/07_Fire_Beetle_Queen.png',
+  './DnD_Monster_Stat_Cards_10/08_Mushroom_Brute.png',
+  './DnD_Monster_Stat_Cards_10/09_Stone_Guardian.png',
+  './DnD_Monster_Stat_Cards_10/10_Goblin_Chief.png',
+
+  './dnd20_species_headshots_v15/aarakocra.png',
+  './dnd20_species_headshots_v15/aasimar.png',
+  './dnd20_species_headshots_v15/dragonborn.png',
+  './dnd20_species_headshots_v15/dwarf.png',
+  './dnd20_species_headshots_v15/elf.png',
+  './dnd20_species_headshots_v15/firbolg.png',
+  './dnd20_species_headshots_v15/genasi.png',
+  './dnd20_species_headshots_v15/gnome.png',
+  './dnd20_species_headshots_v15/goblin.png',
+  './dnd20_species_headshots_v15/goliath.png',
+  './dnd20_species_headshots_v15/half_elf.png',
+  './dnd20_species_headshots_v15/half_orc.png',
+  './dnd20_species_headshots_v15/halfling.png',
+  './dnd20_species_headshots_v15/human.png',
+  './dnd20_species_headshots_v15/kenku.png',
+  './dnd20_species_headshots_v15/kobold.png',
+  './dnd20_species_headshots_v15/tabaxi.png',
+  './dnd20_species_headshots_v15/tiefling.png',
+  './dnd20_species_headshots_v15/triton.png',
+  './dnd20_species_headshots_v15/yuan_ti.png'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil((async()=>{
+    const cache=await caches.open(CACHE_NAME);
+    await cache.addAll(PRECACHE.map(url=>new Request(url,{cache:'reload'})));
+    await self.skipWaiting();
+  })());
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    const names=await caches.keys();
+    await Promise.all(names.filter(name=>name.startsWith('rb600-offline-')&&name!==CACHE_NAME).map(name=>caches.delete(name)));
+    await self.clients.claim();
+  })());
+});
+
+async function networkFirst(request){
+  const cache=await caches.open(CACHE_NAME);
+  try{
+    const response=await fetch(request);
+    if(response&&response.ok) await cache.put(request,response.clone());
+    return response;
+  }catch(err){
+    const cached=await cache.match(request,{ignoreSearch:true});
+    if(cached) return cached;
+    throw err;
+  }
+}
+
+async function cacheFirst(request){
+  const cache=await caches.open(CACHE_NAME);
+  const cached=await cache.match(request,{ignoreSearch:true});
+  if(cached) return cached;
+  const response=await fetch(request);
+  if(response&&response.ok) await cache.put(request,response.clone());
+  return response;
+}
+
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.method!=='GET') return;
+
+  const url=new URL(request.url);
+  if(url.origin!==self.location.origin) return;
+
+  const networkPreferred=
+    request.mode==='navigate'||
+    request.destination==='document'||
+    request.destination==='script'||
+    request.destination==='style'||
+    request.destination==='worker';
+
+  event.respondWith((async()=>{
+    try{
+      return networkPreferred ? await networkFirst(request) : await cacheFirst(request);
+    }catch(err){
+      if(request.mode==='navigate'){
+        const cache=await caches.open(CACHE_NAME);
+        return (await cache.match(request,{ignoreSearch:true})) || (await cache.match('./index.html'));
+      }
+      throw err;
+    }
+  })());
+});
